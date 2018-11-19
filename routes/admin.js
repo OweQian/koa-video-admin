a03f2fa1e8b89c41c3ed4c71abd63a1d8585e493
王倩 <1510106069@qq.com>
2018-11-19 19:03:26 +0800

    feat: 管理员 手机用户 评论 喜欢/不喜欢 middleware

--- routes/admin.js
+++ routes/admin.js
@@ -18,13 +18,15 @@ router.get('/', async (ctx, next) => {
   await apiModel.findData('videos').then(res => {
     dataLength = res.length
   })
-  await apiModel.findPageData('videos', page, 7).then(res => {
+  await apiModel.findPageData('videos', page, 15).then(res => {
     data = JSON.parse(JSON.stringify(res))
+    console.log(res)
   })
   await ctx.render('videoList', {
     videos: data,
     session: ctx.session,
-    dataLength: Math.ceil(dataLength / 7),
+    type: 'videoList',
+    dataLength: Math.ceil(dataLength / 15),
     nowPage: parseInt(page)
   })
 })
@@ -91,142 +93,96 @@ router.post('/upload', async (ctx, next) => {
     }
   })
 })
-// 列表
-router.get('/list', async (ctx, next) => {
-  // ctx.router available
-  await ctx.render('videoList', {
-    session: {
-      user: '王小白'
-    },
-    dataLength: 5,
-    nowPage: 1,
-    type: 'list',
-    videos: [{
-      name: '战狼',
-      country: '中国',
-      classify: '电影',
-      release_time: '2018-11-12',
-      image: '',
-      star: '8',
-      duration: '350',
-      type: '1',
-      actors: '武警',
-      detail: '巴拉芭芭拉'
-    }, {
-      name: '战狼',
-      country: '中国',
-      classify: '电影',
-      release_time: '2018-11-12',
-      image: '',
-      star: '8',
-      duration: '350',
-      type: '1',
-      actors: '武警',
-      detail: '巴拉芭芭拉'
-    }]
-  })
-})
-
-// 上传数据
-router.get('/upload', async (ctx, next) => {
-  await ctx.render('upload', {
-    session: {
-      user: '王小白'
-    }
-  })
-})
 
 // 管理员列表
 router.get('/adminUser', async (ctx, next) => {
+  let page, dataLength = '', data
+  if (ctx.querystring === '') {
+    page = 1
+  } else {
+    page = ctx.querystring.split('=')[1]
+  }
+  await apiModel.findData('admin_users').then(res => {
+      dataLength = res.length
+  })
+  await apiModel.findPageData('admin_users', page, 15).then(res => {
+    data = res
+  })
   await ctx.render('adminUser', {
-    session: {
-      user: '王小白'
-    },
-    dataLength: 5,
-    nowPage: 1,
+    session: ctx.session,
+    dataLength: Math.ceil(dataLength / 15),
+    nowPage: parseInt(page),
     type: 'adminUser',
-    users: [{
-      id: 1,
-      username: 'wangxiaobai'
-    }, {
-      id: 2,
-      username: 'wangxiaobai'
-    }]
+    users: data
   })
 })
 
 // 手机用户列表
 router.get('/mobileUser', async (ctx, next) => {
+  let page, dataLength = '', data
+  if (ctx.querystring === '') {
+    page = 1
+  } else {
+    page = ctx.querystring.split('=')[1]
+  }
+  await apiModel.findData('mobile_users').then(res => {
+    dataLength = res.length
+  })
+  await apiModel.findPageData('mobile_users', page, 15).then(res => {
+    data = res
+  })
   await ctx.render('mobileUser', {
-    session: {
-      user: '王小白'
-    },
-    dataLength: 5,
-    nowPage: 1,
+    session: ctx.session,
+    dataLength: Math.ceil(dataLength / 15),
+    nowPage: parseInt(page),
     type: 'mobileUser',
-    users: [{
-      id: 1,
-      username: 'wangxiaobai',
-      avator: ''
-    }, {
-      id: 2,
-      username: 'wangxiaobai',
-      avator: ''
-    }]
+    users: data
   })
 })
 
 // 评论列表
 router.get('/comments', async (ctx, next) => {
+  let page, dataLength = '', data
+  if (ctx.querystring === '') {
+    page = 1
+  } else {
+    page = ctx.querystring.split('=')[1]
+  }
+  await apiModel.findData('comments').then(res => {
+    dataLength = res.length
+  })
+  await apiModel.findPageData('comments', page, 15).then(res => {
+    data = res
+  })
   await ctx.render('comments', {
-    session: {
-      user: '王小白'
-    },
-    dataLength: 5,
-    nowPage: 1,
+    session: ctx.session,
+    dataLength: Math.ceil(dataLength / 15),
+    nowPage: parseInt(page),
     type: 'comments',
-    comments: [{
-      id: 1,
-      username: 'wangxiaobai',
-      date: '2018-11-13',
-      content: '可不高峰',
-      video_name: '战狼',
-      uid: 1
-    }, {
-      id: 2,
-      username: 'wangxiaobai',
-      date: '2018-11-13',
-      content: '可不高峰',
-      video_name: '战狼',
-      uid: 2
-    }]
+    comments: data
   })
 })
 
 // 喜欢、不喜欢
 router.get('/favorites', async (ctx, next) => {
+  let page, dataLength = '', data
+  if (ctx.querystring === '') {
+    page = 1
+  } else {
+    page = ctx.querystring.split('=')[1]
+  }
+  await apiModel.findData('favorites').then(res => {
+    dataLength = res.length
+  })
+  await apiModel.findPageData('favorites', page, 15).then(res => {
+    data = res
+  })
   await ctx.render('favorites', {
-    session: {
-      user: '王小白'
-    },
-    dataLength: 5,
-    nowPage: 1,
+    session: ctx.session,
+    dataLength: Math.ceil(dataLength / 15),
+    nowPage: parseInt(page),
     type: 'favorites',
-    favorites: [{
-      id: 1,
-      is_favorite: '喜欢',
-      username: 'wangxiaobai',
-      video_name: '战狼',
-      star: '7.8',
-      uid: 1
-    }, {
-      id: 2,
-      is_favorite: '喜欢',
-      username: 'wangxiaobai',
-      video_name: '战狼',
-      star: '7.8',
-      uid: 1
-    }]
+    favorites: data
   })
 })
 
