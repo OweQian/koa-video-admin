59c1f4badfb8a01a13caa143acda853e39a32aad
王倩 <1510106069@qq.com>
2018-11-26 00:05:29 +0800

    feat: 电影列表 编辑 删除

--- config/index.js
+++ config/index.js
@@ -1,14 +1,14 @@
 // const { env } = process
 module.exports = {
   // 端口号
-  PORT: '8000',
+  PORT: 3000,
   // 数据库配置
   DATABASE: {
     NAME: 'koaSql',
     USERNAME: 'root',
     PASSWORD: '123456',
     PORT: '3306',
-    HOST: '39.107.239.100'
+    HOST: 'localhost'
   },
   JWT_SECRET: 'U2FsdGVkX18JEG5lPJBBeaWMGgFgvkvYUSFl7zC4MdQ='
 }
