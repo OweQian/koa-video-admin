ce4fa5813dab97ff6b7509c3a96885247ccf7674
王倩 <1510106069@qq.com>
2018-11-26 23:25:19 +0800

    feat: 格式化

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const logger = require('koa-logger')
 const compress = require('koa-compress')
 const koaBody = require('koa-body')
 const StaticCache = require('koa-static-cache')
+// koa-body
+const getUploadFileExt = require('./utils/getUploadFileExt')
+const getUploadFileName = require('./utils/getUploadFileName')
+const checkDirExist = require('./utils/checkDirExist')
+const getUploadDirName = require('./utils/getUploadDirName')
 
 const app = new Koa()
 const route = new Router()
@@ -22,6 +27,31 @@ let store = new MysqlStore({
   database: config.DATABASE.NAME,
   host: config.DATABASE.HOST
 })
+
+let KoaBody = {
+  multipart: true, // 支持文件上传
+  formidable: {
+    uploadDir: path.join(__dirname, './public/upload'), // 设置文件上传目录
+    keepExtensions: true, // 保持文件后缀名
+    maxFieldsSize: 2 * 1024 * 1024, // 文件上传大小
+    onFileBegin: (name, file) => {
+      // console.log(file);
+      // 获取文件后缀
+      const ext = getUploadFileExt(file.name)
+      // 最终要保存到的文件夹目录
+      const dirName = getUploadDirName()
+      const dir = path.join(__dirname, `public/upload/${dirName}`)
+      // 检查文件夹是否存在如果不存在则新建文件夹
+      checkDirExist(dir)
+      // 获取文件名称
+      const fileName = getUploadFileName(ext)
+      // 重新覆盖 file.path 属性
+      file.path = `${dir}/${fileName}`
+      app.context.uploadpath = app.context.uploadpath ? app.context.uploadpath : {}
+      app.context.uploadpath[name] = `${dirName}/${fileName}`
+    }
+  }
+}
 
 app.use(logger())
 app.use(cors())
@@ -51,37 +81,7 @@ app.use(views(path.join(__dirname, './views'), {
 // compress
 app.use(compress({threshold: 2048}))
 
-// koa-body
-const getUploadFileExt = require('./utils/getUploadFileExt')
-const getUploadFileName = require('./utils/getUploadFileName')
-const checkDirExist = require('./utils/checkDirExist')
-const getUploadDirName = require('./utils/getUploadDirName')
-
-app.use(koaBody({
-    multipart: true, // 支持文件上传
-    formidable: {
-      uploadDir: path.join(__dirname, './public/upload'), // 设置文件上传目录
-      keepExtensions: true, // 保持文件后缀名
-      maxFieldsSize: 2 * 1024 * 1024, // 文件上传大小
-      onFileBegin: (name, file) => {
-        // console.log(file);
-        // 获取文件后缀
-        const ext = getUploadFileExt(file.name)
-        // 最终要保存到的文件夹目录
-        const dirName = getUploadDirName()
-        const dir = path.join(__dirname, `public/upload/${dirName}`)
-        // 检查文件夹是否存在如果不存在则新建文件夹
-        checkDirExist(dir)
-        // 获取文件名称
-        const fileName = getUploadFileName(ext)
-        // 重新覆盖 file.path 属性
-        file.path = `${dir}/${fileName}`
-        app.context.uploadpath = app.context.uploadpath ? app.context.uploadpath : {}
-        app.context.uploadpath[name] = `${dirName}/${fileName}`
-      }
-    }
-  }
-))
+app.use(koaBody(KoaBody))
 
 app.use(require('./routes/admin').routes()).use(route.allowedMethods())
 
